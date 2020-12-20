646ceb2e134f99b369870a9bcd9ca1b68433ef8f
Akshat srivastava <sri303akshat@gmail.com>
2020-12-21 02:33:36 +0530

    PHP-Project

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+ <footer class="foo">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:17px;"><i class="material-icons" style="color: brown;">COPYRIGHT</i> 2020-21 <b >Online Notes Sharing <a href="https://projectworlds.in">mr_cs.coder</a></b></p>
+                </div>
+            </div>
+   </footer>
